6564bc23d33b8b5ba333e3ecf303cc3767e9840d
Baymurat <robbi92@mail.ru>
2018-10-11 01:37:14 +0200

    Implementing File manage service

--- Contacts/web/src/main/java/com/itechart/contacts/web/CustomRestController.java
+++ Contacts/web/src/main/java/com/itechart/contacts/web/CustomRestController.java
@@ -1,10 +1,12 @@
 package com.itechart.contacts.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.itechart.contacts.core.entities.Attachment;
 import com.itechart.contacts.core.entities.Contact;
 import com.itechart.contacts.core.entities.Message;
 import com.itechart.contacts.core.service.SimpleService;
 import com.itechart.contacts.core.utils.Result;
+import com.itechart.utils.FileManageService;
 import org.springframework.http.MediaType;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +21,7 @@ import java.util.Map;
 public class CustomRestController {
 
     private SimpleService simpleService = new SimpleService();
+    private FileManageService fileManageService = new FileManageService();
 
     @RequestMapping(value = "/get-contacts", method = RequestMethod.GET)
     public Result getContacts(@RequestParam(name = "from")  int from, @RequestParam(name = "range") int range) {
@@ -32,34 +35,28 @@ public class CustomRestController {
 
     @RequestMapping(value = "/add-record", method = RequestMethod.POST)
     public void addRecord(@RequestParam("contact") String jsonRepresentation, @RequestPart("files") MultipartFile[] files) {
-        System.out.println(files.length);
-        System.out.println(jsonRepresentation);
-        ObjectMapper mapper = new ObjectMapper();
-        Contact contact = null;
-        try {
-            contact = mapper.readValue(jsonRepresentation, Contact.class);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Contact contact = parseToContact(jsonRepresentation);
         simpleService.addRecord(contact);
-
+        //remove null below
+        fileManageService.uploadFiles(files, null,contact.getId());
     }
-
-
-    /*@RequestMapping(value = "/add-record", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public void addRecord(@RequestParam("contact") Contact contact) {
-        simpleService.addRecord(contact);
-    }*/
-
 
     @RequestMapping(value = "/delete-record", method = RequestMethod.POST)
     public void deleteRecord(@RequestBody int[] deleteContactsId) {
         simpleService.deleteRecord(deleteContactsId);
+        fileManageService.deleteUsers(deleteContactsId);
     }
 
     @RequestMapping(value = "/update-record", method = RequestMethod.POST)
-    public void updateRecord(@RequestBody Contact contact) {
+    public void updateRecord(@RequestParam("contact") String jsonRepresentation, @RequestPart("files") MultipartFile[] files) {
+        Contact contact = parseToContact(jsonRepresentation);
         simpleService.updateRecord(contact);
+        List<Integer> attachmentList = contact.getDeleteAttachmentsList();
+        if (attachmentList.size() > 0) {
+            fileManageService.deleteFiles(contact.getId(), attachmentList);
+        }
+        //fix below
+        fileManageService.uploadFiles(files, null, contact.getId());
     }
 
     @RequestMapping(value = "/search-contact", method = RequestMethod.GET)
@@ -70,5 +67,16 @@ public class CustomRestController {
     @RequestMapping(value = "/send-email", method = RequestMethod.POST)
     public void sendEmail(@RequestBody Message message) {
         simpleService.sendEmail(message);
+    }
+
+    private Contact parseToContact(String jsonRepresentation) {
+        ObjectMapper mapper = new ObjectMapper();
+        Contact contact = null;
+        try {
+            contact = mapper.readValue(jsonRepresentation, Contact.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return contact;
     }
 }
---
+++ Contacts/web/src/main/java/com/itechart/contacts/web/UploadFile.java
@@ -0,0 +1,8 @@
+package com.itechart.contacts.web;
+
+public class UploadFile {
+    public int length;
+    public byte[] bytes;
+    public String name;
+    public String type;
+}
